491e96e7d19e7393f79ec6f80f7d1eca2d99c04d
Steve Zimmers <steve@soccer-1.com>
2017-12-08 11:30:38 -0500

    add search bar stub

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = '';
+
 const App = () => {
-	return <div>yo</div>;
+	return (
+		<div>
+			<SearchBar/>
+		</div>
+	);
 };
 
 ReactDOM.render(<App/>, document.querySelector('.container'));
