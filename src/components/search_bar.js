5c488791c63b0c14755d1e5db0bda82a7c6a537b
Steve Zimmers <steve@soccer-1.com>
2017-12-08 12:06:15 -0500

    change search bar to class based component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, {Component} from 'react';
 
+// functional component
+/*
 const SearchBar = () => {
 	return <input/>;
 };
+*/
+
+// class-based component. can introspect, hold state, communicate w/ other components.
+// render() required.
+class SearchBar extends Component {
+	render() {
+		return <input />;
+	}
+}
 
 export default SearchBar;
