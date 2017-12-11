e91bb15a0c8fd06520f1c9141d2cd8c72c93b6af
Steve Zimmers <steve@soccer-1.com>
2017-12-11 13:39:41 -0500

    implement click to select video

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -4,7 +4,12 @@ import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
 	const videoItems = props.videos.map((video) => {
-		return <VideoListItem key={video.etag} video={video} />
+		return (
+			<VideoListItem key={video.etag}
+						   onVideoSelect={props.onVideoSelect}
+						   video={video}
+			/>
+		)
 	});
 
 	return (
