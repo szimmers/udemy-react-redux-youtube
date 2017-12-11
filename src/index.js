8b4caeef736ddcb6a1c4211c1b8838c980378221
Steve Zimmers <steve@soccer-1.com>
2017-12-11 11:45:32 -0500

    - change app from function to class based component - make questionable change to move search to w/in ctor

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
@@ -6,16 +6,24 @@ import SearchBar from './components/search_bar';
 
 const API_KEY = '';
 
-YTSearch({key: API_KEY, term: 'larry'}, function(data) {
-	console.log(data);
-});
+class App extends Component {
+	constructor(props) {
+		super(props);
 
-const App = () => {
-	return (
-		<div>
-			<SearchBar/>
-		</div>
-	);
+		this.state = {videos: []};
+
+		YTSearch({key: API_KEY, term: 'larry'}, (videos) => {
+			this.setState({videos});
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<SearchBar/>
+			</div>
+		);
+	}
 };
 
 ReactDOM.render(<App/>, document.querySelector('.container'));
