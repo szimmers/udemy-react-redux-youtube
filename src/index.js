db8512bee32d72f9ae4d63461f850061d82ed4c5
Steve Zimmers <steve@soccer-1.com>
2017-12-11 11:31:06 -0500

    add stub for searching

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = '';
+
+YTSearch({key: API_KEY, term: 'larry'}, function(data) {
+	console.log(data);
+});
 
 const App = () => {
 	return (
